96308cd60d16cd3e33ad34386d7eee2fc7d1130f
Eugene Shalaev <anarxict97@mail.ru>
2025-11-11 19:38:51 +1000

    таблица для фильмов, перенастроил стайлинт

--- src/app/cinemas/page.tsx
+++ src/app/cinemas/page.tsx
@@ -1,30 +1,46 @@
-import classNames from "classnames";
 import styles from "./styles.module.scss";
 
 export default function Page() {
   return (
-    <section
-      id="movies"
-      className={classNames(styles.contentSection, styles.active)}
-    >
-      <h2 className={styles.secondaryTitle}>Сейчас в прокате</h2>
-      <div className={styles.movieGrid}>
-        <div className={styles.movieCard}>
-          <h3>Аватар 2</h3>
-          <p>Фантастика, Приключения</p>
-          <p>⭐ 4.8/5</p>
-        </div>
-        <div className={styles.movieCard}>
-          <h3>Оппенгеймер</h3>
-          <p>Драма, Биография</p>
-          <p>⭐ 4.9/5</p>
-        </div>
-        <div className={styles.movieCard}>
-          <h3>Барби</h3>
-          <p>Комедия, Приключения</p>
-          <p>⭐ 4.7/5</p>
-        </div>
-      </div>
+    <section id="cinemas" className={styles.contentSection}>
+      <table className={styles.table}>
+        <thead className={styles.tableHead}>
+          <tr className={styles.row}>
+            <th className={styles.captionHead} scope="col">
+              Кинотеатр
+            </th>
+            <th className={styles.captionHead} scope="col">
+              Адрес
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className={styles.row}>
+            <th className={styles.head} scope="row">
+              Skyline Cinema
+            </th>
+            <td className={styles.data} scope="row">
+              ТРЦ Галерея
+            </td>
+          </tr>
+          <tr className={styles.row}>
+            <th className={styles.head} scope="row">
+              Салют
+            </th>
+            <td className={styles.data} scope="row">
+              пр-т Рокоссовского 6
+            </td>
+          </tr>
+          <tr className={styles.row}>
+            <th className={styles.head} scope="row">
+              Океан
+            </th>
+            <td className={styles.data} scope="row">
+              Алеутская 8
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   );
 }
